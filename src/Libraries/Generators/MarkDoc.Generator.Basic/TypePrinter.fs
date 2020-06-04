b2dd8a69ef84d1c9ac64a9e92cac8275ed596567
Denis Akopyan <denis.akopyan@seznam.cz>
2020-06-04 16:06:28 +0200

    #23 - Add summary extractor & property section

--- src/Libraries/Generators/MarkDoc.Generator.Basic/TypePrinter.fs
+++ src/Libraries/Generators/MarkDoc.Generator.Basic/TypePrinter.fs
@@ -15,6 +15,55 @@ type TypePrinter(creator, resolver, linker) =
   let m_resolver : IDocResolver    = resolver
   let m_linker   : ILinker         = linker
 
+  let toElement x = x :> IElement 
+  let textNormal x = m_creator.CreateText(x, IText.TextStyle.Normal)
+  let textBold x = m_creator.CreateText(x, IText.TextStyle.Bold)
+  let textItalic x = m_creator.CreateText(x, IText.TextStyle.Italic)
+  let textCode x = m_creator.CreateText(x, IText.TextStyle.Code)
+  let textInline x = m_creator.CreateText(x, IText.TextStyle.CodeInline)
+
+  let summaryShort (x : ITag) =
+    let getCount = 
+      let isInvalid (item : IContent) =
+        match item with
+        | :? IListTag -> true
+        | :? IInnerTag as tag ->
+          match tag.Type with
+          | IInnerTag.InnerTagType.Para
+          | IInnerTag.InnerTagType.Code
+          | IInnerTag.InnerTagType.InvalidTag -> true
+          | _ -> false
+        | _ -> false
+
+      match (x.Content |> Seq.tryFindIndex isInvalid) with
+      | None -> x.Content.Count
+      | Some x -> x
+
+    let processContent (item : IContent) = 
+      match item with
+      | :? ITextTag as text -> Some(textNormal text.Content :> ITextContent)
+      | :? IInnerTag as inner ->
+        match inner.Type with
+        //| IInnerTag.InnerTagType.CodeSingle -> textInline inner.Content |> Seq.exactlyOne
+        | IInnerTag.InnerTagType.ParamRef
+        | IInnerTag.InnerTagType.TypeRef -> Some(textInline inner.Reference :> ITextContent)
+        | IInnerTag.InnerTagType.See
+        | IInnerTag.InnerTagType.SeeAlso -> Some(textBold inner.Reference :> ITextContent) // TODO: Create link
+        | _ -> None
+      | _ -> None
+
+    let content = x.Content
+                  |> Seq.take getCount
+                  |> Seq.map processContent
+                  |> Seq.filter(fun x -> x <> None)
+                  |> Seq.map(fun x -> x.Value)
+    m_creator.JoinTextContent(content, " ")
+
+  let memberNameSummary(name : ITextContent, summary : Option<ITag>) =
+    match summary with
+    | None -> name
+    | Some x -> m_creator.JoinTextContent(seq [ name; summaryShort x ], "<br>")
+
   let findTag(input : IType, mem : IMember, tag : ITag.TagType) = 
     let mutable typeDoc : IDocElement = null
     if not (m_resolver.TryFindType(input, &typeDoc)) then
@@ -33,7 +82,7 @@ type TypePrinter(creator, resolver, linker) =
   let printMemberTables(input : IInterface) =
     let createHeadings headings =
       headings
-      |> Seq.map m_creator.CreateText
+      |> Seq.map textNormal
 
     let groupMembers (members : seq<'M> when 'M :> IMember) =
       let byStatic(x : 'M) =
@@ -51,7 +100,7 @@ type TypePrinter(creator, resolver, linker) =
       let createRow(method : IMethod) =
         let processReturn =
           let name = if isNull method.Returns then "void" else method.Returns.DisplayName
-          let content = m_creator.CreateText(name, IText.TextStyle.CodeInline)
+          let content = textInline name
           if isNull method.Returns then
             content :> ITextContent
           else
@@ -61,20 +110,19 @@ type TypePrinter(creator, resolver, linker) =
             else
               m_creator.CreateLink(content, link) :> ITextContent
 
-        let processMethod : ITextContent = 
+        let processMethod = 
           let hasOverloads =
             methodsArray
             |> Seq.where(fun x -> x.Name = method.Name)
             |> Seq.skip 1
             |> Seq.isEmpty
+
           let signature = method.Name + "(" + (if hasOverloads then "..." else "()") + ")"
-          let summary = findTag(input, method, ITag.TagType.Summary) |> Seq.tryExactlyOne
+          let signatureText = textInline signature
+          memberNameSummary(signatureText, findTag(input, method, ITag.TagType.Summary) |> Seq.tryExactlyOne)
 
-          let signatureText = m_creator.CreateText(signature, IText.TextStyle.CodeInline)
-          m_creator.JoinTextContent([signatureText], "<br>")
-
-        [ processReturn; processMethod ]
-        |> Seq.map(fun x -> x :> IElement)
+        seq [ processReturn; processMethod ]
+        |> Seq.map toElement
         |> Linq.ToReadOnlyCollection
 
       let grouped = methodsArray
@@ -85,6 +133,46 @@ type TypePrinter(creator, resolver, linker) =
 
       m_creator.CreateTable(grouped, [ "Returns"; "Name" ] |> createHeadings, "TODO", 3)
 
+    let createPropertySection(isStatic, accessor, properties : seq<IProperty>) =
+      let createRow(property : IProperty) =
+        let processType =
+          let content = textInline property.Type.DisplayName
+          let link = m_linker.CreateLink(property.Type)
+          if (System.String.IsNullOrEmpty link) then
+            content :> ITextContent
+          else
+            m_creator.CreateLink(content, link) :> ITextContent
+
+        let processName = 
+          let name = textInline property.Name :> ITextContent
+          memberNameSummary(name, findTag(input, property, ITag.TagType.Summary) |> Seq.tryExactlyOne)
+
+        let processMethods = 
+          let getter = 
+            if property.GetAccessor.HasValue then
+              seq [ textInline "get" :> ITextContent ]
+            else
+              Seq.empty
+          let setter = 
+            if property.SetAccessor.HasValue then
+              seq [ textInline "set" :> ITextContent ]
+            else
+              Seq.empty
+
+          m_creator.JoinTextContent(Seq.append getter setter, " ")
+
+        seq [ processType; processName; processMethods ]
+        |> Seq.map toElement
+        |> Linq.ToReadOnlyCollection
+
+      let grouped = properties
+                    |> Seq.groupBy(fun x -> x.Name)
+                    |> Seq.sortBy fst
+                    |> Seq.collect snd
+                    |> Seq.map createRow
+
+      m_creator.CreateTable(grouped, [ "Type"; "Name"; "Methods" ] |> createHeadings, "TODO", 3)
+
     let processMembers (item) =
       let flatten item =
         let a = fst item
@@ -94,13 +182,20 @@ type TypePrinter(creator, resolver, linker) =
       |> Seq.map flatten
       |> Seq.collect(fun x -> x)
 
-    let a = input.Methods
-            |> groupMembers
-            |> processMembers
-            |> Seq.map createMethodSection
-            |> Seq.map(fun x-> x :> IElement)
+    let methods = input.Methods
+                  |> groupMembers
+                  |> processMembers
+                  |> Seq.map (createMethodSection >> toElement)
 
-    [ m_creator.CreateSection(a, "Methods", 2) :> IElement ]
+    let properties = input.Properties
+                     |> groupMembers
+                     |> processMembers
+                     |> Seq.map (createPropertySection >> toElement)
+
+    seq [
+      m_creator.CreateSection(methods, "Methods", 2) :> IElement;
+      m_creator.CreateSection(properties, "Properties", 2) :> IElement;
+    ]
 
   interface ITypePrinter with
     member __.Print(input : IInterface) = 
