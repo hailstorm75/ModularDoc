485e4163107f6ca5d6548501685c1ac8ae5e73e5
Deno <denis.akopyan@seznam.cz>
2020-07-28 14:36:25 +0200

    #77 - Implement relative linking support

--- src/Libraries/Linkers/MarkDoc.Linkers.Markdown/LinkGenerator.fs
+++ src/Libraries/Linkers/MarkDoc.Linkers.Markdown/LinkGenerator.fs
@@ -1,12 +1,40 @@
 ﻿namespace MarkDoc.Linkers.Markdown
 
 open MarkDoc.Members.Types
+open System
 open System.Collections.Generic
 
 module private Link =
+  let private processTarget source target =
+    if source = target then
+      target
+    else
+      let split (input: string) =
+        input.Split("/", StringSplitOptions.RemoveEmptyEntries)
+      let foldersSource = split source
+      let foldersTarget = split target
+
+      let index =
+        seq [
+          for i in 0 .. min foldersSource.Length foldersTarget.Length do
+            yield foldersSource.[i].Equals(foldersTarget.[i], StringComparison.Ordinal)
+        ]
+        |> Seq.findIndex not
+
+      let cdUp =
+        seq [
+          for _ in 0 .. index - 1 do
+            yield "../"
+
+          for i in index .. foldersTarget.Length - 1 do
+            yield foldersTarget.[i]
+        ]
+
+      String.Join(String.Empty, cdUp)
+
   let createLink(source: IType, target: IType, structure: IReadOnlyDictionary<IType, string>, platform: GitPlatform) =
-    let mutable result = null
-    if structure.TryGetValue(target, &result) then
-      result
+    let mutable resultTarget, resultSource = null, null
+    if structure.TryGetValue(target, &resultTarget) && structure.TryGetValue(source, &resultSource) then
+      processTarget resultSource resultTarget
     else
       ""
