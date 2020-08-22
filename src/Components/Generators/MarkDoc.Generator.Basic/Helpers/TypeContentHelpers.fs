edd3208e89292ed3d237657d012021d7e77a71b5
Denis Akopyan <denis.akopyan@seznam.cz>
2020-08-22 20:56:38 +0200

    #91 - Document the TypeContentHelpers

--- src/Components/Generators/MarkDoc.Generator.Basic/Helpers/TypeContentHelpers.fs
+++ src/Components/Generators/MarkDoc.Generator.Basic/Helpers/TypeContentHelpers.fs
@@ -1,28 +1,70 @@
 ﻿namespace MarkDoc.Generator.Basic
 
-open MarkDoc.Members.Types
-open MarkDoc.Elements
 open MarkDoc.Documentation.Tags
-open System
 open MarkDoc.Members.Members
+open MarkDoc.Members.Types
 open MarkDoc.Members.Enums
-open System.Collections.Generic
 open MarkDoc.Members
+open MarkDoc.Elements
 open MarkDoc.Helpers
+open System.Collections.Generic
+open System
 
+/// <summary>
+/// Documentation section types for types
+/// </summary>
 type TypeContentType =
+  /// <summary>
+  /// Type summary documentation
+  /// </summary>
   | TypeSummary
+  /// <summary>
+  /// Type remarks documentation
+  /// </summary>
   | TypeRemarks
+  /// <summary>
+  /// Type example documentation
+  /// </summary>
   | TypeExample
+  /// <summary>
+  /// Type generics and their constraints documentation
+  /// </summary>
   | TypeGenerics
+  /// <summary>
+  /// Type inherited types documentation
+  /// </summary>
   | TypeInheritance
+  /// <summary>
+  /// Type nested types documentation
+  /// </summary>
   | TypeNested
+  /// <summary>
+  /// Type additional references
+  /// </summary>
   | TypeSeeAlso
+  /// <summary>
+  /// Type constructors documentation
+  /// </summary>
   | TypeConstructors
+  /// <summary>
+  /// Type methods documentation
+  /// </summary>
   | TypeMethods
+  /// <summary>
+  /// Type properties documentation
+  /// </summary>
   | TypeProperties
+  /// <summary>
+  /// Type events documentation
+  /// </summary>
   | TypeEvents
+  /// <summary>
+  /// Type delegates documentation
+  /// </summary>
   | TypeDelegates
+  /// <summary>
+  /// Enum fields documentation
+  /// </summary>
   | TypeFields
 
 module TypeContentHelpers =
@@ -57,11 +99,18 @@ module TypeContentHelpers =
     | _ -> raise (NotSupportedException())
 
   let private single x input tools =
+    // Get the documentation tag for given type
     let tag = TagHelpers.findTypeTag input x tools |> Seq.tryExactlyOne
+    // If no tag was found..
     if Option.isNone tag then
+      // return nothing
       None
+    // Otherwise..
     else
-      TagHelpers.tagFull input (tag |> Option.get) tools |> SomeHelpers.emptyToNone
+      // process the tag into content
+      TagHelpers.tagFull input (tag |> Option.get) tools
+      // if there is no content return nothing
+      |> SomeHelpers.emptyToNone
   let private genericProcessor provider processItem tools =
     let processItems (items: 'M IReadOnlyCollection when 'M :> IMember) =
       items
@@ -83,19 +132,24 @@ module TypeContentHelpers =
   let private processContent (item: (IMember -> Tools -> IElement seq option) * string) (mem: IMember) tools =
     let applyMember input = input mem
     let applyTools input = input tools
+    // Process content and repackage
     (item |> (fst >> applyMember >> applyTools), snd item)
 
   let private joinContentWSig content signature (input: IType) tools (mem: IMember) =
     let applyMember input = input mem
     let applyTools input = input tools
-    let processContents (content: IType -> seq<(IMember -> Tools -> IElement seq option) * string>) =
-      content input |> Seq.map processContent
+    let processContents content = content input |> Seq.map processContent
 
+    // Get documentation sections
     let toProcess = processContents (content |> ContentHelpers.processContents)
+                    // Supply argument values
                     |> Seq.map (applyMember >> applyTools)
 
+    // Compose documentation sections
     composeSections toProcess 4
+    // Supply argument values for each section
     |> Seq.map (applyTools >> ElementHelpers.toElement)
+    // Append the composed signature section
     |> Seq.append (seq [ signature |> applyMember |> TextHelpers.processText |> applyTools |> ElementHelpers.toElement ])
 
   let private overloads (members: 'M IReadOnlyCollection when 'M :> IMember) =
@@ -116,120 +170,175 @@ module TypeContentHelpers =
       // return nothing
       String.Empty
   let private getArgumentsSig (input: IType) tools (mem: IMember) =
-    methodArguments input mem tools |> TextHelpers.processTextNoStyle
+    // Compose the method arguments
+    methodArguments input mem tools
+    // Materialize to unstylized text
+    |> TextHelpers.processTextNoStyle
 
   let private nested (input: IType) tools =
     let applyTools input = input tools
-    let getNested (x: IInterface) = x.NestedTypes
+    let getNested (input: IInterface) = input.NestedTypes
 
-    let groupByType (x: IType) =
-      match x with
-      | :? IClass
-        -> "c" |> Some
-      | :? IStruct
-        -> "s" |> Some
-      | :? IInterface
-        -> "i" |> Some
-      | :? IEnum
-        -> "e" |> Some
-      | _ -> None
+    let groupByType (itemType: IType) =
+      match itemType with
+      | :? IClass     -> "Classes"    |> Some
+      | :? IStruct    -> "Structures" |> Some
+      | :? IInterface -> "Interfaces" |> Some
+      | :? IEnum      -> "Enums"      |> Some
+      | _             -> None
 
-    let processGroup (x: string option * seq<IType>) =
-      let createTable heading group =
-        ListElement (group |> Seq.map (fun x -> ElementHelpers.initialize (TypeHelpers.getTypeName x |> InlineCode |> TextElement) tools), IList.ListType.Dotted, heading, 3)
-        |> Some
-
-      match x |> (fst >> Option.get) with
-      | "c" -> createTable "Classes" (snd x)
-      | "i" -> createTable "Interfaces" (snd x)
-      | "s" -> createTable "Structures" (snd x)
-      | "e" -> createTable "Enums" (snd x)
-      | _ -> None
+    let processGroup (grouping: string * seq<IType>) =
+      // Processes a type to an element
+      let processType itemType = ElementHelpers.initialize (TypeHelpers.getTypeName itemType |> InlineCode |> TextElement) tools
+      // Create a list from the elements
+      ListElement (grouping |> snd |> Seq.map processType, IList.ListType.Dotted, fst grouping, 3) |> Some
 
     match input with
-    | :? IInterface as x ->
-       x
+    | :? IInterface as inter ->
+       inter
+       // Get the nested types
        |> getNested
+       // Group nested types
        |> Seq.groupBy groupByType
-       |> Seq.filter (fst >> Option.isSome)
+       // Filter out invalid types
+       |> SomeHelpers.whereSome2
+       // Compose content for each group
        |> Seq.map processGroup
+       // Filter out invalid types
        |> SomeHelpers.whereSome
+       // Compose content
        |> Seq.map (ElementHelpers.initialize >> applyTools)
+       // If there is no content return nothing
        |> SomeHelpers.emptyToNone
     | _ -> None
 
   let private inheritance (input: IType) tools =
     let getInterfaces (x: 'M when 'M :> IInterface) =
+      // Get the inherited interfaces
       x.InheritedInterfaces
+      // Compose the inherited types into elements
       |> Seq.map (fun x -> ElementHelpers.initialize (TypeHelpers.processResType input x tools |> TextElement) tools)
 
-    let createList l =
-      if (Seq.isEmpty l) then
+    let createList elements =
+      // If there are no elements..
+      if (Seq.isEmpty elements) then
+        // return nothing
         None
+      // Otherwise..
       else
-        seq [ tools.creator.CreateList(l, IList.ListType.Dotted) |> ElementHelpers.toElement ] |> Some
+        // compose a list
+        seq [ tools.creator.CreateList(elements, IList.ListType.Dotted) |> ElementHelpers.toElement ] |> Some
 
+    // Process the input based on its type
     match input with
-    | :? IClass as x ->
-      let baseType = if (isNull x.BaseClass) then None else ElementHelpers.initialize (TypeHelpers.processResType input x.BaseClass tools |> TextElement) tools |> Some
-      let interfaces = getInterfaces x
+    | :? IClass as classType ->
+      // Get the inherited base class
+      let baseType =
+        // If there is no base class..
+        if (isNull classType.BaseClass) then
+          // return nothing
+          None
+        // Otherwise..
+        else
+          // compose the base class element
+          ElementHelpers.initialize (TypeHelpers.processResType input classType.BaseClass tools |> TextElement) tools |> Some
+
+      // Get the inherited interfaces
+      let interfaces = getInterfaces classType
+
       seq [ baseType ]
+      // Exclude the base class if it is missing
       |> SomeHelpers.whereSome
+      // Append the interfaces to the inherited types sequence
       |> Seq.append interfaces
+      // Compose the list of elements
       |> createList
-    | :? IInterface as x ->
-      x |> (getInterfaces >> createList)
+    | :? IInterface as interfaceType ->
+      interfaceType
+      // Compose the list of elemets
+      |> (getInterfaces >> createList)
     | _ -> None
 
   let private typeParams (input: IType) tools =
     let applyTools input = input tools
+    // Get the type generics
     let getTypeParams =
-      let processTag (x: ITag) =
+      let processTag (tag: ITag) =
+        // Get the type generics
         let generics = (input :?> IInterface).Generics
-        let getConstraints (x: ITag) =
-          if generics.ContainsKey(x.Reference) then
-            let types = generics.[x.Reference].ToTuple()
+        let getConstraints (tag: ITag) =
+          // If the given tag references an existing generic type..
+          if generics.ContainsKey(tag.Reference) then
+            // Get the generic constraints
+            let types = generics.[tag.Reference].ToTuple()
+                        // Get the generics constraint types
                         |> snd
-                        |> Seq.map (fun x -> TypeHelpers.processResType input x tools)
+                        // Compose generic type constraints
+                        |> Seq.map (fun constraintType -> TypeHelpers.processResType input constraintType tools)
+            // Return the result followed by a newline
             (types, Environment.NewLine) |> JoinedText |> Some
+          // Otherwise..
           else
+            // return nothing
             None
 
-        let getName (x: ITag) =
+        let getName (tag: ITag) =
+          // Get the generic constraint signature
           let result = seq [
-            yield x.Reference |> InlineCode
-            if (generics.ContainsKey(x.Reference)) then
-              let variance = generics.[x.Reference].ToTuple() |> fst
+            // Get the constraint name
+            yield tag.Reference |> InlineCode
+            // If the given tag references an existing generic type..
+            if (generics.ContainsKey(tag.Reference)) then
+              // Get the constraint variance type
+              let variance = generics.[tag.Reference].ToTuple() |> fst
+              // if the constraint is a variant type..
               if (variance <> Enums.Variance.NonVariant) then
+                // print the variance type
                 yield variance |> StringConverters.varianceStr |> InlineCode
           ]
+          // Join the text
           (result, " ") |> JoinedText
 
-        let constraints = getConstraints x
+        // Get the generic constraints documentation
+        let constraints = getConstraints tag
         seq [
-          yield getName x
-          yield TagHelpers.tagShort input x tools
+          // Get the generic constraint name
+          yield getName tag
+          // Get the documention
+          yield TagHelpers.tagShort input tag tools
 
+          // If there are any constraints..
           if (Option.isSome constraints) then
+            // return them
             yield constraints |> Option.get
         ]
+        // Compose to elements
         |> Seq.map (TextHelpers.processText >> applyTools >> ElementHelpers.toElement)
 
+      // If the input type can have type constraints..
       if input :? IInterface then
+        // Get the documentation for the given input type
         TagHelpers.findTypeTag input ITag.TagType.Typeparam tools
+        // Process each generic type and its constraints
         |> Seq.map (processTag >> Linq.ToReadOnlyCollection)
         |> Some
+      // Otherwise..
       else
+        // return nothing
         None
 
-    let ts = getTypeParams
-    if (Option.isSome ts && ts |> (Option.get >> Seq.isEmpty >> not)) then
-      seq [ tools.creator.CreateTable(ts |> Option.get, createHeadings (seq [ "Type"; "Description"; "Constraints" ]) tools) |> ElementHelpers.toElement ] |> Some
+    // If there are generics..
+    if (Option.isSome getTypeParams && getTypeParams |> (Option.get >> Seq.isEmpty >> not)) then
+      // create a table of generics documentation
+      seq [ tools.creator.CreateTable(getTypeParams |> Option.get, createHeadings (seq [ "Type"; "Description"; "Constraints" ]) tools) |> ElementHelpers.toElement ] |> Some
+    // Otherwise..
     else
+      // return nothing
       None
 
   let private constructors (input: IType) tools =
     let extractor =
+      // If the input is a class, return the constructors. Otherwise return an empty collection
       match input with
       | :? IClass as x -> x.Constructors
       | _ -> LinkedList<IConstructor>() :> IReadOnlyCollection<IConstructor>
@@ -242,6 +351,7 @@ module TypeContentHelpers =
           getArgumentsSig input tools
         ])
 
+      // Define the content to be documented for this member
       let content =
         seq [
           Arguments;
@@ -254,10 +364,12 @@ module TypeContentHelpers =
 
       (ctor, ctor.Name + overloadFormat extractor.Count i, joinContentWSig content signature input)
 
+    // Process the composed data
     genericProcessor extractor processCtor tools
 
   let private methods (input: IType) tools =
     let extractor =
+      // If the input can have methods, return the methods. Otherwise return an empty collection
       match input with
       | :? IInterface as x -> x.Methods
       | _ -> new LinkedList<IMethod>() :> IReadOnlyCollection<IMethod>
@@ -279,6 +391,7 @@ module TypeContentHelpers =
           getArgumentsSig input tools
         ])
 
+      // Define the content to be documented for this member
       let content =
         seq [
           Arguments;
@@ -293,10 +406,12 @@ module TypeContentHelpers =
 
       (method, (if method.Operator <> OperatorType.None then "Operator " else "") + method.Name + getOverloads, joinContentWSig content signature input)
 
+    // Process the composed data
     genericProcessor extractor processMethod tools
 
   let private properties (input: IType) tools =
     let extractor = 
+      // If the input can have properties, return the properties. Otherwise return an empty collection
       match input with
       | :? IInterface as x -> x.Properties
       | _ -> new LinkedList<IProperty>() :> IReadOnlyCollection<IProperty>
@@ -310,6 +425,8 @@ module TypeContentHelpers =
           SignatureHelpers.getName;
           SignatureHelpers.getPropertyMethods
         ])
+
+      // Define the content to be documented for this member
       let content =
         seq [
           Summary;
@@ -323,10 +440,12 @@ module TypeContentHelpers =
 
       (property, property.Name, joinContentWSig content signature input)
 
+    // Process the composed data
     genericProcessor extractor processProperty tools
 
   let private events (input: IType) tools =
     let extractor = 
+      // If the input can have events, return the events. Otherwise return an empty collection
       match input with
       | :? IInterface as x -> x.Events
       | _ -> new LinkedList<IEvent>() :> IReadOnlyCollection<IEvent>
@@ -338,6 +457,8 @@ module TypeContentHelpers =
           SignatureHelpers.getReturn;
           SignatureHelpers.getName
         ])
+
+      // Define the content to be documented for this member
       let content =
         seq [
           Summary;
@@ -350,10 +471,12 @@ module TypeContentHelpers =
 
       (event, event.Name, joinContentWSig content signature input)
 
+    // Process the composed data
     genericProcessor extractor processEvent tools
 
   let private delegates (input: IType) tools =
     let extractor = 
+      // If the input can have delegates, return the delegates. Otherwise return an empty collection
       match input with
       | :? IInterface as x -> x.Delegates
       | _ -> new LinkedList<IDelegate>() :> IReadOnlyCollection<IDelegate>
@@ -372,6 +495,7 @@ module TypeContentHelpers =
           getArgumentsSig input tools
         ])
 
+      // Define the content to be documented for this member
       let content =
         seq [
           Arguments;
@@ -385,15 +509,19 @@ module TypeContentHelpers =
 
       (deleg, deleg.Name + getOverloads, joinContentWSig content signature input)
 
+    // Process the composed data
     genericProcessor extractor processDelegate tools
 
   let private enumFields (input: IType) tools =
     let extractor =
+      // If the input is an enum, return its fields. Otherwise return an empty collection
       match input with
       | :? IEnum as e -> e.Fields
       | _ -> new LinkedList<IEnumField>() :> IReadOnlyCollection<IEnumField>
     let processField (_, field: IEnumField) =
       let applyTools input = input tools
+
+      // Define the content to be documented for this member
       let content =
         let sections =
           seq [
@@ -409,11 +537,13 @@ module TypeContentHelpers =
         composeSections processed 4
         |> Seq.map applyTools
 
+      // Note: Unecessary evil to comply with the generic program flow
       let intermediate tools _ =
         (if Seq.isEmpty content then seq [ TextHelpers.empty tools |> ElementHelpers.toElement ] else content)
 
       (field, field.Name, intermediate)
 
+    // Process the composed data
     genericProcessor extractor processField tools
 
   let processContents input tools content =
