04b3320ba76baae2d069296d16259ac165657dca
Denis Akopyan <denis.akopyan@seznam.cz>
2020-09-28 23:38:25 +0200

    #131 - Add static tests

--- src/Tests/UnitTests/Components/UT.Members/MemberTests/DelegateTests.cs
+++ src/Tests/UnitTests/Components/UT.Members/MemberTests/DelegateTests.cs
@@ -16,7 +16,8 @@ namespace UT.Members.MemberTests
 
     public static IEnumerable<object[]> GetDelegateNameData()
     {
-      var filter = new HashSet<string> { Constants.DELEGATES_CLASS, Constants.DELEGATES_STRUCT, Constants.DELEGATES_INTERFACE };
+      var filter = new HashSet<string>
+        {Constants.DELEGATES_CLASS, Constants.DELEGATES_STRUCT, Constants.DELEGATES_INTERFACE};
 
       foreach (var container in new ResolversProvider())
       {
@@ -27,7 +28,7 @@ namespace UT.Members.MemberTests
           .OfType<IInterface>()
           .Where(type => filter.Contains(type.Name));
         foreach (var type in types ?? throw new Exception())
-          yield return new object[] { type, Constants.DELEGATE_PUBLIC };
+          yield return new object[] {type, Constants.DELEGATE_PUBLIC};
       }
     }
 
@@ -46,10 +47,26 @@ namespace UT.Members.MemberTests
         var resolver = container.First() as IResolver;
         resolver?.Resolve(Constants.TEST_ASSEMBLY);
 
-        var result = resolver?.Types.Value[Constants.DELEGATES_NAMESPACE].OfType<IClass>().First(type => type.Name.Equals(Constants.DELEGATES_CLASS));
-        object?[] typeWrapper = { result };
+        var result = resolver?.Types.Value[Constants.DELEGATES_NAMESPACE]
+          .OfType<IClass>()
+          .First(type => type.Name.Equals(Constants.DELEGATES_CLASS));
+        object?[] typeWrapper = {result};
         foreach (object?[] entry in data)
           yield return typeWrapper.Concat(entry).ToArray();
+      }
+    }
+
+    public static IEnumerable<object?[]> GetDelegateData()
+    {
+      foreach (var container in new ResolversProvider())
+      {
+        var resolver = container.First() as IResolver;
+        resolver?.Resolve(Constants.TEST_ASSEMBLY);
+
+        var result = resolver?.Types.Value[Constants.DELEGATES_NAMESPACE].OfType<IClass>();
+        foreach (var type in result ?? throw new Exception())
+          foreach (var @delegate in type.Delegates)
+            yield return new object[] {type, @delegate.Name};
       }
     }
 
@@ -58,7 +75,7 @@ namespace UT.Members.MemberTests
       var data = new object[]
       {
         new object[] {Constants.DELEGATE_GENERIC, ("TDelegate", new string[] { })},
-        new object[] {Constants.DELEGATE_GENERIC_CONSTRAINT, ("TDelegate", new [] {nameof(IDisposable)})}
+        new object[] {Constants.DELEGATE_GENERIC_CONSTRAINT, ("TDelegate", new[] {nameof(IDisposable)})}
       };
 
       foreach (var container in new ResolversProvider())
@@ -87,6 +104,17 @@ namespace UT.Members.MemberTests
 
     #endregion
 
+
+    [Theory]
+    [Trait("Category", nameof(IDelegate))]
+    [MemberData(nameof(GetDelegateData))]
+    public void ValidateDelegateIsStatic(IInterface type, string name)
+    {
+      var member = GetDelegate(type, name, true);
+
+      Assert.False(member?.IsStatic);
+    }
+
     [Theory]
     [Trait("Category", nameof(IDelegate))]
     [MemberData(nameof(GetDelegateNameData))]
@@ -110,7 +138,8 @@ namespace UT.Members.MemberTests
     [Theory]
     [Trait("Category", nameof(IDelegate))]
     [MemberData(nameof(GetDelegateGenericsData))]
-    public void ValidateDelegateGenericNames(IInterface type, string name, (string generic, string[] constraints) generics)
+    public void ValidateDelegateGenericNames(IInterface type, string name,
+      (string generic, string[] constraints) generics)
     {
       var members = GetDelegate(type, name, true);
       var generic = members?.Generics.First();
@@ -121,7 +150,8 @@ namespace UT.Members.MemberTests
     [Theory]
     [Trait("Category", nameof(IDelegate))]
     [MemberData(nameof(GetDelegateGenericsData))]
-    public void ValidateDelegateGenericConstraints(IInterface type, string name, (string generic, string[] constraints) generics)
+    public void ValidateDelegateGenericConstraints(IInterface type, string name,
+      (string generic, string[] constraints) generics)
     {
       var members = GetDelegate(type, name, true);
       var generic = members?.Generics.First();
