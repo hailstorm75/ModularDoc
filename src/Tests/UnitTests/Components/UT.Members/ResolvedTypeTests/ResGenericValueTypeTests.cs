018d3974045f36f20b3d060557207f1ad25a4df5
Denis Akopyan <denis.akopyan@seznam.cz>
2020-11-14 13:01:34 +0100

    #154 - Write unit tests for ResGenericValueTypes

---
+++ src/Tests/UnitTests/Components/UT.Members/ResolvedTypeTests/ResGenericValueTypeTests.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using MarkDoc.Helpers;
+using MarkDoc.Members.Members;
+using MarkDoc.Members.ResolvedTypes;
+using MarkDoc.Members.Types;
+using UT.Members.Data;
+using Xunit;
+
+namespace UT.Members.ResolvedTypeTests
+{
+  public class ResGenericValueTypeTests
+  {
+    #region Data provider
+
+    public static IEnumerable<object[]> GetGenericValueTypeNames()
+    {
+      var data = new object[]
+      {
+        new object[] {Constants.METHOD_RES_GENERIC_VALUE_TYPES, new[] { "T1", "T2" }},
+        new object[] {Constants.METHOD_RES_GENERIC_OWN_VALUE_TYPES, new[] { "TA", "TB" }},
+        new object[] {Constants.METHOD_RES_GENERIC_MIXED_VALUE_TYPES, new[] { "T1", "TA", "T2", "TB" }},
+      };
+
+      foreach (var resolver in new ResolversProvider().WhereNotNull())
+      {
+        resolver.Resolve(Constants.TEST_ASSEMBLY);
+
+        var parent = resolver.FindMemberParent<IClass>(Constants.RES_TYPES_NAMESPACE, Constants.RES_TYPE_GENERIC_VALUE_TYPE_CLASS);
+        object?[] typeWrapper = {parent};
+        foreach (object?[] entry in data)
+          yield return typeWrapper.Concat(entry).ToArray()!;
+      }
+    }
+
+    private static IMethod? GetMethod(IInterface type, string name, bool throwIfNull = false)
+      => type.Methods.FindMember(name, throwIfNull);
+
+    #endregion
+
+    [Theory]
+    [Trait("Category", nameof(IResType))]
+    [MemberData(nameof(GetGenericValueTypeNames))]
+    public void ValidateValueTypeIsRef(IInterface type, string name, string[] names)
+    {
+      var member = GetMethod(type, name, true);
+
+      Assert.Equal(names, member?.Arguments.Select(argument => argument.Type.DisplayName).ToArray());
+    }
+  }
+}
---
+++ tests/TestLibrary/ResTypes/ClassResGenericValueTypes.cs
@@ -0,0 +1,17 @@
+﻿namespace TestLibrary.ResTypes
+{
+  public class ClassResGenericValueTypes<T1, T2>
+  {
+    public void MethodGenericValueTypes(T1 a, T2 b)
+    {
+    }
+
+    public void MethodGenericOwnValueTypes<TA, TB>(TA a, TB b)
+    {
+    }
+
+    public void MethodGenericMixedValueTypes<TA, TB>(T1 a, TA b, T2 c, TB d)
+    {
+    }
+  }
+}
