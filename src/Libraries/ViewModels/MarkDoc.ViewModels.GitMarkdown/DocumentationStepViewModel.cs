f9b0c0975934b1d7762eade54fc0bf4d5650ea71
Denis Akopyan <denis.akopyan@seznam.cz>
2021-11-24 22:44:03 +0100

    Prepare Documentation step

--- src/Libraries/ViewModels/MarkDoc.ViewModels.GitMarkdown/DocumentationStepViewModel.cs
+++ src/Libraries/ViewModels/MarkDoc.ViewModels.GitMarkdown/DocumentationStepViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using MarkDoc.Documentation;
 using MarkDoc.MVVM.Helpers;
 
@@ -7,11 +8,38 @@ namespace MarkDoc.ViewModels.GitMarkdown
   public sealed class DocumentationStepViewModel
     : BaseStepViewModel<IDocSettings>
   {
+    #region Constants
+
+    private const string SETTINGS_PATHS = "paths";
+    private const char PATH_DELIM = '|';
+
+    #endregion
+
+    #region Properties
+
     /// <inheritdoc />
     public override string Title => "Documentation extraction settings";
 
     /// <inheritdoc />
     public override string Description => "TODO";
+
+    /// <summary>
+    /// Paths to documentation
+    /// </summary>
+    public ObservableCollection<string> Paths { get; } = new();
+
+    /// <summary>
+    /// Missing paths to documentation
+    /// </summary>
+    public ObservableCollection<string> MissingPaths { get; } = new();
+
+    #endregion
+
+    /// <summary>
+    /// Default constructor
+    /// </summary>
+    public DocumentationStepViewModel()
+      => MissingPaths.CollectionChanged += (_, _) => UpdateCanProceed();
 
     /// <inheritdoc />
     public override void SetNamedArguments(IReadOnlyDictionary<string, string> arguments)
@@ -21,6 +49,14 @@ namespace MarkDoc.ViewModels.GitMarkdown
 
     /// <inheritdoc />
     public override IReadOnlyDictionary<string, string> GetSettings()
-      => new Dictionary<string, string>();
+      => new Dictionary<string, string>
+      {
+        {
+          SETTINGS_PATHS, string.Join(PATH_DELIM, Paths)
+        }
+      };
+
+    private void UpdateCanProceed()
+      => IsValid = MissingPaths.Count > 0;
   }
 }
