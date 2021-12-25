d3c7aff3824f758af9e6394dcc352dc0ba4dbf61
Denis Akopyan <denis.akopyan@seznam.cz>
2021-12-25 20:30:13 +0100

    Create inhouse logger

---
+++ src/Libraries/Core/MarkDoc.Core/IMarkDocLogger.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace MarkDoc.Core
+{
+  public interface IMarkDocLogger
+  {
+    public enum LogType
+    {
+      Info,
+      Error,
+      Warning,
+      Debug
+    }
+
+    public readonly struct LogMessage
+    {
+      /// <summary>
+      /// Log type
+      /// </summary>
+      public LogType Type { get; }
+
+      /// <summary>
+      /// Log message
+      /// </summary>
+      public string Message { get; }
+
+      /// <summary>
+      /// Log time
+      /// </summary>
+      public DateTime Time { get; }
+
+      /// <summary>
+      /// Default constructor
+      /// </summary>
+      public LogMessage(LogType type, string message)
+      {
+        Type = type;
+        Message = message;
+        Time = DateTime.Now;
+      }
+    }
+
+    event EventHandler<LogMessage> NewLog;
+
+    void Info(string message);
+    void Debug(string message);
+    void Error(string message);
+    void Warning(string message);
+  }
+}
