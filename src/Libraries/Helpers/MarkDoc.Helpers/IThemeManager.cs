cbc2392799beeba5588841420d58c31225a3fc07
Denis Akopyan <denis.akopyan@seznam.cz>
2022-05-23 09:51:16 +0200

    #209 - Implement dark mode switching

---
+++ src/Libraries/Helpers/MarkDoc.Helpers/IThemeManager.cs
@@ -0,0 +1,10 @@
+﻿namespace MarkDoc.Helpers
+{
+  public interface IThemeManager
+  {
+    bool GetDarkMode();
+    void SetDarkMode(bool enabled);
+    void SaveThemeSettings();
+    void LoadThemeSettings();
+  }
+}
--- src/Libraries/ViewModels/MarkDoc.ViewModels.Main/SettingsViewModel.cs
+++ src/Libraries/ViewModels/MarkDoc.ViewModels.Main/SettingsViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using System.Windows.Input;
+using Avalonia;
 using MarkDoc.Constants;
+using MarkDoc.Helpers;
 using MarkDoc.MVVM.Helpers;
 using ReactiveUI;
 
@@ -8,7 +10,27 @@ namespace MarkDoc.ViewModels.Main
   public class SettingsViewModel
     : BaseViewModel, ISettingsViewModel
   {
+    #region Field
+
     private readonly NavigationManager m_navigationManager;
+    private readonly IThemeManager m_themeManager;
+    private bool m_isDarkModeOn;
+
+    #endregion
+
+    /// <summary>
+    /// Determines whether the dark mode is on
+    /// </summary>
+    public bool IsDarkModeOn
+    {
+      get => m_isDarkModeOn;
+      set
+      {
+        m_isDarkModeOn = value;
+        SetDarkMode(value);
+        this.RaisePropertyChanged();
+      }
+    }
 
     /// <inheritdoc />
     public ICommand BackCommand { get; set; }
@@ -16,13 +38,21 @@ namespace MarkDoc.ViewModels.Main
     /// <summary>
     /// Default constructor
     /// </summary>
-    public SettingsViewModel(NavigationManager navigationManager)
+    public SettingsViewModel(NavigationManager navigationManager, IThemeManager themeManager)
     {
       m_navigationManager = navigationManager;
+      m_themeManager = themeManager;
       BackCommand = ReactiveCommand.Create(NavigateBack);
+      IsDarkModeOn = m_themeManager.GetDarkMode();
     }
 
+    private void SetDarkMode(bool enabled)
+      => m_themeManager.SetDarkMode(enabled);
+
     private void NavigateBack()
-      => m_navigationManager.NavigateTo(PageNames.HOME);
+    {
+      m_themeManager.SaveThemeSettings();
+      m_navigationManager.NavigateTo(PageNames.HOME);
+    }
   }
 }
